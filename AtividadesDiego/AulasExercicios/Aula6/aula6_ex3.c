3238a5c3d486a35d8a1b6b2b8bd4d0713829133f
eduardo citron <joaoguidelli54@gmail.com>
2026-07-28 19:47:20 -0300

    correção para linux e adição da lista 2

--- AtividadesDiego/AulasExercicios/Aula6/aula6_ex3.c
+++ AtividadesDiego/AulasExercicios/Aula6/aula6_ex3.c
@@ -2,31 +2,34 @@
 
 typedef struct {
     char nome[50];
-    float nota;    
+    float nota;
 } infoAluno;
 
 int main() {
     setbuf(stdout, NULL);
-    int n, iMaior = 0, iMenor;
-    scanf("%d", &n);
-    infoAluno aluno[n];
-    printf("Digite o numero de alunos: ");
     
+    int n;
+    printf("Digite o numero de alunos: ");
+    scanf("%d", &n);    
+    infoAluno aluno[n];
+    int iMaior = 0;
+    int iMenor = 0; 
 
     for (int i = 0; i < n; i++) {
-        printf("Digite o nome do aluno: \n");
+        printf("Digite o nome do aluno %d: \n", i + 1);
         scanf("%s", aluno[i].nome);
-        printf("Digite a notas do aluno: \n");
-        scanf("%f", aluno[i].nota);
-        if (aluno[i].nota > aluno[iMaior].nota) { 
+        printf("Digite a nota do aluno: \n");
+        scanf("%f", &aluno[i].nota); 
+        if (aluno[i].nota > aluno[iMaior].nota) {
             iMaior = i;
-        } else if (aluno[i].nota < aluno[iMenor].nota) {
+        }
+        if (aluno[i].nota < aluno[iMenor].nota) {
             iMenor = i;
-        }     
+        }
     }
 
-    printf("Maior nota: %.2f", aluno[iMaior].nota);
-    printf("\nMenor nota: %.2f", aluno[iMenor].nota); 
-    return 0;
+    printf("\nMaior nota: %.2f (Aluno: %s)", aluno[iMaior].nota, aluno[iMaior].nome);
+    printf("\nMenor nota: %.2f (Aluno: %s)\n", aluno[iMenor].nota, aluno[iMenor].nome);
 
+    return 0;
 }
